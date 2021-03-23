fa2e12acb14a561586e3815340c77efd0ec6f1bf
jrluka13 <56405398+jrluka13@users.noreply.github.com>
2021-03-23 21:43:45 +0300

    change app.js

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.use(express.json({extended: true}))
 app.use('/api/auth',require('./routes/auth_routes'))
 app.use('/api/book',require('./routes/book.routes'))
 
-if(process.env.NODE_ENV === 'production'){
+// if(process.env.NODE_ENV === 'production'){
   app.use('/',express.static(path.join(__dirname,'client','build')))
 
   app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
-}
+// }
 
-const PORT = process.env.PORT || config.get("port") 
+const PORT = process.env.PORT || config.get("port")
 
 async function start() {
   try {
